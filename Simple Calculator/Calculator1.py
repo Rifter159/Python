d4ca1f1445d04e97e81c184b359ae8a8084d3843
SOVA00159 <73084865+SOVA00159@users.noreply.github.com>
2020-10-28 17:19:34 +0530

    Comment
    symbol change

--- Simple Calculator/Calculator1.py
+++ Simple Calculator/Calculator1.py
@@ -3,5 +3,6 @@ print("Good name " + user_name)
 num1 = input("Enter a number:")
 num2 = input("Enter another number:")
 solution = float(num1) + float(num2) 
+# You can even substract ,multiply ,divide by changing the symbol above
 
 print( solution ) 
