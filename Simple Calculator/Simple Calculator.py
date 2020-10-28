956949cb5256de676100c7d5a4633784cb2666a5
SOVA00159 <73084865+SOVA00159@users.noreply.github.com>
2020-10-28 12:33:25 +0530

    Create Simple Calculator.py

---
+++ Simple Calculator/Simple Calculator.py
@@ -0,0 +1,7 @@
+# A simple calculator by vishalss1
+print("Good name " + user_name)
+num1 = input("Enter a number:")
+num2 = input("Enter another number:")
+solution = float(num1) + float(num2) 
+
+print( solution ) 
