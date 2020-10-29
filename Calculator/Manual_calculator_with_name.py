e795d3951bf2500512d27866b4dd35b626ce9581
SOVA00159 <73084865+SOVA00159@users.noreply.github.com>
2020-10-29 11:51:09 +0530

    Create Manual_calculator_with_name.py

---
+++ Calculator/Manual_calculator_with_name.py
@@ -0,0 +1,11 @@
+# A simple calculator 
+print("Hello there this is a simple calculator, type two numbers to get started")
+user_name = input("Enter your name:")
+print("Nice name " + user_name)
+num1 = input("Enter a number:")
+num2 = input("Enter another number:")
+solution = float(num1) + float(num2) 
+# You can even substract ,multiply ,divide by changing the symbol above 
+
+print( solution ) 
+print("This is the answer " + user_name + "!" )
